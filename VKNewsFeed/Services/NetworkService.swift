e3e6f3af90101919682c23856046444f6ef04643
Maxim Spiridonov <taijased@gmail.com>
2019-05-12 18:52:27 +0300

    add network logic

--- VKNewsFeed/Services/NetworkService.swift
+++ VKNewsFeed/Services/NetworkService.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
-final class NetworkService {
+protocol Networking {
+    func request(path: String, params: [String : String], completion: @escaping  (Data?, Error?) -> Void)
+}
+final class NetworkService: Networking {
     
     private let authService: AuthService
     
@@ -18,19 +21,28 @@ final class NetworkService {
         self.authService = authService
     }
 
-    func getFeed() {
-       
-        
+    func request(path: String, params: [String : String], completion: @escaping (Data?, Error?) -> Void) {
         guard let token = authService.token else { return }
         let params = ["filters": "post, photo"]
         var allParams = params
         allParams["access_token"] = token
         allParams["v"] = API.version
         
-        let url = self.url(from: API.newsFeed, params: allParams)
-       
-        print(url)
+        let url = self.url(from: path, params: allParams)
+        let session = URLSession.init(configuration: .default)
+        let request = URLRequest(url: url)
+        let task = createDataTask(from: request, completion: completion)
+        task.resume()
     }
+
+    private func createDataTask(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        })
+    }
+
     
     private func url(from path: String, params: [String: String]) -> URL {
         var components = URLComponents()
